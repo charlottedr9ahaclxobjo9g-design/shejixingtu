82ad93e9f1b3e59fa3fc03f6cd7a762b40f0f82a
charlottedr9ahaclxobjo9g-design <design@shejixingtu.cn>
2026-05-22 11:28:37 +0800

    v2: trailingSlash fix + feishu form link + nojekyll

--- _next/static/chunks/app/resources/page-eb3dd02b3e1e65c3.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[126,160,301,222,444,791,732,618,931],{5125:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=5125)}),_N_E=n.O()}]);
