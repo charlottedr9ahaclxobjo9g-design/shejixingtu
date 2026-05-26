0e5ddfe3cebf54ff060efea8456e7357ceccf470
charlottedr9ahaclxobjo9g-design <design@shejixingtu.cn>
2026-05-26 09:21:07 +0800

    fix: v3.2 公司城市去重 + 作品集横向卡片/序号 + 资料卡充实

---
+++ _next/static/chunks/app/skills/[slug]/page-efeb533da74c6a7b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[791,160,618,222,931,732,301],{5125:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=5125)}),_N_E=n.O()}]);
